d45720bb4bfb97762a722a4c42dd87693bfb73ec
johnyMicky <166741409+johnyMicky@users.noreply.github.com>
2026-04-04 20:37:40 +0400

    Fix storageBucket URL in firebase configuration

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,12 +7,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyCSfBcYpTKfuTzKO_56JBtyBgQqXiggvM4",
   authDomain: "morganex-60185.firebaseapp.com",
   projectId: "morganex-60185",
-  storageBucket: "morganex-60185.firebasestorage.app",
+  storageBucket: "morganex-60185.appspot.com",
   messagingSenderId: "417098187610",
   appId: "1:417098187610:web:9df5e07cc627c4f6212963"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
