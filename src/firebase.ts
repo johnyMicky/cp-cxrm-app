b5c3354c5740aef696a330cba09d7d6fae6a92b3
johnyMicky <166741409+johnyMicky@users.noreply.github.com>
2026-04-04 21:33:41 +0400

    Update Firebase storage bucket URL

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCSfBcYpTKfuTzKO_56JBtyBgQqXiggvM4",
   authDomain: "morganex-60185.firebaseapp.com",
   projectId: "morganex-60185",
-  storageBucket: "morganex-60185.appspot.com",
+  storageBucket: "morganex-60185.firebasestorage.app",
   messagingSenderId: "417098187610",
   appId: "1:417098187610:web:9df5e07cc627c4f6212963"
 };
